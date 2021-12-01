6f50e101841ead36df186e78e121e13b3005ad4a
tibocodes <thibault.folly@gmail.com>
2021-12-01 17:12:22 +0000

    day1p1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,4 +1,22 @@
+use std::fs::File;
+use std::io::{self, BufRead};
 
-fn main() {
-  println!("Hello, day 1 !")
+fn main() -> io::Result<()> {
+    let f = File::open("inputs/day1/input")?;
+    let f = io::BufReader::new(f);
+
+    let mut prev: Option<i32> = None;
+    let mut count = 0;
+    for line in f.lines() {
+        let current: i32 = line.unwrap().parse().unwrap();
+
+        if let Some(x) = prev {
+            if current > x {
+                count += 1;
+            }
+        }
+        prev = Some(current);
+    }
+    println!("count: {}", count);
+    Ok(())
 }
