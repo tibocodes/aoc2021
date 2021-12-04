bd616428ce6a997c2437f480beeb6051c2c669f5
tibocodes <thibault.folly@gmail.com>
2021-12-04 15:46:02 +0000

    day4part1

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -1,10 +1,34 @@
 fn main() {
     // dbg!(numbers(INPUT));
     // dbg!(Grid::new(GRID_INPUT));
-    dbg!(Grid::new(GRID_INPUT).won(&[22, 13, 17, 11, 0]));
-    dbg!(Grid::new(GRID_INPUT).won(&[22, 13, 17, 11, 1]));
-    dbg!(Grid::new(GRID_INPUT).won(&[22, 8, 21, 6, 1]));
-    dbg!(Grid::new(GRID_INPUT).won(&[22, 9, 21, 6, 1]));
+    // dbg!(Grid::new(GRID_INPUT).won(&[22, 13, 17, 11, 0]));
+    // dbg!(Grid::new(GRID_INPUT).won(&[22, 13, 17, 11, 1]));
+    // dbg!(Grid::new(GRID_INPUT).won(&[22, 8, 21, 6, 1]));
+    // dbg!(Grid::new(GRID_INPUT).won(&[22, 9, 21, 6, 1]));
+    // dbg!(read_grids(INPUT));
+    dbg!(part_1(INPUT));
+    dbg!(part_1(include_str!("../../inputs/day4")));
+}
+
+fn part_1(input: &str) -> u32 {
+    let grids = read_grids(input);
+    let all_numbers = numbers(input);
+    for i in 1..all_numbers.len() {
+        let played_numbers: Vec<u32> = all_numbers.clone().into_iter().take(i).collect();
+        for grid in &grids {
+            if grid.won(&played_numbers) {
+                println!("{:?}", played_numbers);
+                println!("Winning grid : {:?}", grid.numbers);
+                println!(
+                    "The last number played: {:?}",
+                    played_numbers.last().unwrap()
+                );
+                return played_numbers.last().unwrap()
+                    * grid.sum_of_remaining_numbers(&played_numbers);
+            }
+        }
+    }
+    panic!()
 }
 
 const INPUT: &str = "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
@@ -22,6 +46,13 @@ const INPUT: &str = "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,
 14 21 16 12  6
 
 14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7
+";
+
+const WINNING_GRID: &str = "14 21 17 24  4
 10 16 15  9 19
 18  8 23 26 20
 22 11 13  6  5
@@ -51,6 +82,14 @@ struct Grid {
     numbers: Vec<u32>,
 }
 
+fn read_grids(input: &str) -> Vec<Grid> {
+    input
+        .split("\n\n")
+        .skip(1)
+        .map(|bloc| Grid::new(bloc))
+        .collect()
+}
+
 impl Grid {
     fn new(input_str: &str) -> Grid {
         input_str
@@ -62,12 +101,19 @@ impl Grid {
     }
 
     fn won(&self, played_numbers: &[u32]) -> bool {
+        // if self.won_horizontally(played_numbers) {
+        //     dbg!("won H");
+        // }
+        // if self.won_vertically(played_numbers) {
+        //     dbg!("won V");
+        // }
         self.won_horizontally(played_numbers) || self.won_vertically(played_numbers)
     }
 
     fn won_horizontally(&self, played_numbers: &[u32]) -> bool {
         self.numbers
             .windows(5)
+            .step_by(5)
             .any(|line| line.iter().all(|e| played_numbers.contains(e)))
     }
 
@@ -79,6 +125,13 @@ impl Grid {
                 .step_by(5)
                 .all(|e| played_numbers.contains(e))
         })
+    }
+
+    fn sum_of_remaining_numbers(&self, played_numbers: &[u32]) -> u32 {
+        self.numbers
+            .iter()
+            .filter(|num| !played_numbers.contains(num))
+            .sum()
     }
 }
 
@@ -94,6 +147,10 @@ mod tests {
         );
         assert_eq!(
             Grid::new(GRID_INPUT).won_horizontally(&[22, 13, 17, 11, 1]),
+            false
+        );
+        assert_eq!(
+            Grid::new(GRID_INPUT).won_horizontally(&[7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24]),
             false
         );
     }
@@ -116,5 +173,19 @@ mod tests {
         assert_eq!(Grid::new(GRID_INPUT).won(&[22, 13, 17, 11, 1]), false);
         assert_eq!(Grid::new(GRID_INPUT).won(&[22, 8, 21, 6, 1]), true);
         assert_eq!(Grid::new(GRID_INPUT).won(&[22, 9, 21, 6, 1]), false);
+    }
+
+    #[test]
+    fn test_sum_of_remaining_numbers() {
+        let played_numbers = &[7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24];
+        assert_eq!(
+            Grid::new(WINNING_GRID).sum_of_remaining_numbers(played_numbers),
+            188
+        );
+    }
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(part_1(INPUT), 4512);
     }
 }
